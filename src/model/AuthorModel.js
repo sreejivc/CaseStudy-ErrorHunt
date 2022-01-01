27d4f17f52921860db564b4b5556c7f3f7a2b176
sreejivc <sreejivc@gmail.com>
2022-01-01 21:13:13 +0530

    Case study 2:ErrorHunt

---
+++ src/model/AuthorModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost:27017/Library');//
+mongoose.connect('mongodb+srv://sreeji:sreeji@cluster0.galbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const Schema = mongoose.Schema;
+
+
+const AuthorSchema = new Schema({
+    title : String,
+    image: String,
+    about: String
+});
+
+const authordata = mongoose.model('authordata',AuthorSchema);
+
+module.exports = authordata;
---
+++ src/model/BookModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost:27017/Library');
+mongoose.connect('mongodb+srv://sreeji:sreeji@cluster0.galbd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const Schema = mongoose.Schema;
+
+
+const BookSchema = new Schema({
+    title : String,
+    author: String,
+    image: String,
+    about: String
+});
+
+const bookdata = mongoose.model('bookdata',BookSchema);
+
+module.exports = bookdata;
